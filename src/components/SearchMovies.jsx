d536fbb414b4a44619670d20f01a1f20234d03fb
GonzaSalda <gonza.salda00@gmail.com>
2024-07-29 01:19:20 -0300

    Refactor SearchMovies component

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -1,36 +1,26 @@
-import React from 'react'
-import { useMovieDBContext } from '../context/MovieDBContext'
+import React from "react";
+import { useMovieDBContext } from "../context/MovieDBContext";
 
 const SearchMovies = () => {
-
-  const { query, setQuery } = useMovieDBContext()
+  const { query, setQuery } = useMovieDBContext();
 
   const handleChange = (e) => {
-    setQuery(e.target.value)
-  }
+    setQuery(e.target.value);
+  };
 
   return (
     <>
-
-      <div className='flex justify-center'>
-
+      <div className="flex justify-center">
         <input
           className="my-5 input input-bordered input-success w-full max-w-xs"
           type="text"
           placeholder="Buscar pelicula"
           value={query}
-          onChange={handleChange} />
-
-
-        {/* <input
-      type="text"
-      placeholder="Buscar pelicula"   METODO DE BUSQUEDA SIN PARAMETROS EN LA API
-      onChange={handleChange} /> */}
-
+          onChange={handleChange}
+        />
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default SearchMovies
+export default SearchMovies;
